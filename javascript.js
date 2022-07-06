a1121cfe41d3f30fd82451e5c16192b05273c8ed
christopoulos-g <gr_christopoulos@outlook.com>
2022-07-06 14:08:17 -0400

    stable javascript ver

--- javascript.js
+++ javascript.js
@@ -1,71 +1,94 @@
-const choices_array = ["rock", "paper", "scissors"];
+const rockBtn = document.getElementById("rock_btn");
+const paperBtn = document.getElementById("paper_btn");
+const scissorsBtn = document.getElementById("scissors_btn");
+const roundResult = document.getElementById("round_result");
+const gameResult = document.getElementById("game_result");
+const score = document.getElementById("score");
 
-game();
+let p_score = 0;  /* c_score means computer wins . p_score means player wins . */
+let c_score = 0;
+
+
+rockBtn.addEventListener('click', function() {
+    game('rock');
+});
+paperBtn.addEventListener('click', function() {
+    game('paper');
+});
+scissorsBtn.addEventListener('click', function() {
+    game('scissors');
+});
+
 
 function computerPlay(){
-    let result = 1;
+    const choices_array = ["rock", "paper", "scissors"];
+    let result;
     let random_int = Math.floor(Math.random()*3);
     result = choices_array[random_int];
     return result;
 }
 
-function playRound(playerSelection, computerSelection){
-    let result= "You entered an undefined value! Please choose ONLY between rock, paper and scissors!";
-    console.log(playerSelection, computerSelection);
-    
+
+
+function playRound(playerSelection,computerSelection){
+    let result;
+
     if(computerSelection == "rock" && playerSelection == "rock"){
-        result = "It's a draw!";
+        result = "It's a draw.. No points are awarded!";
     }else if(computerSelection == "rock" && playerSelection == "paper"){
-        result = "Player wins!";
+        result = "Player wins this round!";
     }else if(computerSelection == "rock" && playerSelection == "scissors"){
-        result = "Computer wins!";
+        result = "Computer wins this round!";
     }
 
     if(computerSelection == "paper" && playerSelection == "rock"){
-        result = "Computer wins!";
+        result = "Computer wins this round!";
     }else if(computerSelection == "paper" && playerSelection == "paper"){
-        result = "It's a draw!";
+        result = "It's a draw.. No points are awarded!";
     }else if(computerSelection == "paper" && playerSelection == "scissors"){
-        result = "Player wins!";
+        result = "Player wins this round!";
     }
 
     if(computerSelection == "scissors" && playerSelection == "rock"){
-        result = "Player wins!";
+        result = "Player wins this round!";
     }else if(computerSelection == "scissors" && playerSelection == "paper"){
-        result = "Computer wins!";
+        result = "Computer wins this round!";
     }else if(computerSelection == "scissors" && playerSelection == "scissors"){
-        result = "It's a draw!";
+        result = "It's a draw.. No points are awarded!";
     }    
 
     return result;
 }   
 
-/* c_score means computer wins .
-   p_score means player wins . */
+function gameOver(p_score,c_score){
+    if(p_score == 5 || c_score == 5){
+        return true;
+    }else{
+        return false;
+    }
+}
 
-function game(){
 
-let p_score = 0;
-let c_score = 0;
-const computerSelection = computerPlay();
-    for(let i = 0; i < 5; i++){
-        let input = prompt("Rock, paper or scissors. Pick one!");
-        let playerSelection = input.toLowerCase();
-        alert(`Your choice was ${playerSelection}.`)
+function game(selection){
 
-        playRound();
-        console.log(playRound(playerSelection, computerSelection));
-        if(result = "Player wins!"){
-            p_score++;
-        }else if(result = "Computer wins!"){
-            c_score++;
+    const computerSelection = computerPlay();
+    let playerSelection = selection;
+    let result = playRound(playerSelection,computerSelection);
+
+    if(result == "Player wins this round!"){
+        p_score++;
+    }else if(result == "Computer wins this round!"){
+        c_score++;
+    }
+    score.textContent = p_score + ' + ' + c_score;
+    roundResult.textContent = result ;
+
+    if(gameOver(p_score,c_score) === true){
+        if(p_score > c_score){
+            gameResult.textContent = "You are the winner!";
+        }else {
+            gameResult.textContent = "You lost..";
         }
     }
-    if(p_score++ > c_score){
-        console.log("Best of 5 is the player!")
-    }else if(c_score >> p_score){
-        console.log("Best of 5 is the computer!")
-    }else if (c_score == p_score){
-        console.log("It's a draw...")
-    }
+    return;
 }
